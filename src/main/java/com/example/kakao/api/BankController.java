a191ebebeaa92dc9ab9a4294ff6551dea54aa14f
SIOUkoeran <alstnalsgud@gmail.com>
2022-06-24 18:41:34 +0900

    #2 Feature find All Banks in list

---
+++ src/main/java/com/example/kakao/api/BankController.java
@@ -0,0 +1,34 @@
+package com.example.kakao.api;
+
+
+import com.example.kakao.dto.Response;
+import com.example.kakao.dto.ResponseBank;
+import com.example.kakao.model.Bank;
+import com.example.kakao.repository.BankRepository;
+import com.example.kakao.service.BankService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/bank")
+public class BankController {
+
+    private final BankService bankService;
+
+    @GetMapping("/list")
+    public Response getAllBanks(){
+        List<ResponseBank> allBanks = this.bankService.findAllBanks();
+        return Response.builder()
+                .code("2000")
+                .message("find All Banks")
+                .data(allBanks)
+                .build();
+    }
+}
